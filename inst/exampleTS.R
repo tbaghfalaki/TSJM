8c5f3a3f5f2cbff28511a16d0641510a8d32559f
Taban Baghfalaki <64243070+tbaghfalaki@users.noreply.github.com>
2024-04-11 09:43:44 +0200

    new

--- inst/exampleTS.R
+++ inst/exampleTS.R
@@ -39,12 +39,18 @@ TS0 <- TS(formFixed, formRandom, formGroup, formSurv,
          DIC = TRUE, quiet = FALSE, dataLong_t, dataSurv_t
 )
 
-model <- list("linear", "linear", "linear")
+
+
+formFixed <- list(Y1 ~ obstime, Y2 ~ obstime, Y3 ~ obstime, Y4 ~ obstime)
+formRandom <- list(~obstime, ~obstime, ~obstime, ~obstime)
+formGroup <- list(~id, ~id, ~id, ~id)
+formSurv <- survival::Surv(survtime, death) ~ x1 + x2
+model <- list("linear", "linear", "linear", "linear")
 
 TS1 <- TS(formFixed, formRandom, formGroup, formSurv,
-         nmark = 3, K1 = 15, K2 = 15,
-         model = model, n.chains1 = 1, n.iter1 = 20, n.burnin1 = 10,
-         n.thin1 = 1,  n.chains2 = 1, n.iter2 = 30, n.burnin2 = 10,
+         nmark = 4, K1 = 15, K2 = 15,
+         model = model, n.chains1 = 1, n.iter1 = 2000, n.burnin1 = 1000,
+         n.thin1 = 1,  n.chains2 = 1, n.iter2 = 3000, n.burnin2 = 1000,
          n.thin2 = 1, Obstime = "obstime", ncl = 2,
          DIC = TRUE, quiet = FALSE, dataLong_t, dataSurv_t
 )
@@ -52,16 +58,17 @@ TS1 <- TS(formFixed, formRandom, formGroup, formSurv,
 
 
 DP <- DP(TS1,
-         s = 0.5, t = 0.5, n.chains = 1, n.iter = 20, n.burnin = 10,
+         s = 0.5, t = 0.5, n.chains = 1, n.iter = 2000, n.burnin = 1000,
          n.thin = 1,
          DIC = TRUE, quiet = FALSE, dataLong = dataLong_v, dataSurv = dataSurv_v
 )
 
 
 
-DP <- DP_CI(TS0,
-         s = 0.5, t = 0.5, n.chains = 1, n.iter = 20, n.burnin = 10,
-         n.thin = 1, mi=3,
+
+DP <- DP_CI(TS1,
+         s = 0.1, t = 0.5, n.chains = 1, n.iter = 2000, n.burnin = 1000,
+         n.thin = 1, mi=20,
          DIC = TRUE, quiet = FALSE, dataLong = dataLong_v, dataSurv = dataSurv_v
 )
 
